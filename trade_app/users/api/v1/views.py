994baa572cf4a8391444f88e29fce7bcc16ae395
Kolesnikovich Vladimir <vovakolesnikovich@gmail.com>
2021-01-29 17:22:19 +0300

    Added DefaultRouter

--- trade_app/users/api/v1/views.py
+++ trade_app/users/api/v1/views.py
@@ -1,10 +1,13 @@
-from rest_framework import generics
+from rest_framework.mixins import CreateModelMixin
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import GenericViewSet
+from rest_framework_simplejwt.state import User
 
 from users.api.v1.serializers import UserSerializer
 
 
-class UserView(generics.CreateAPIView):
+class UserViewSet(CreateModelMixin, GenericViewSet):
     """Creates a user instance"""
     permission_classes = (~IsAuthenticated,)
     serializer_class = UserSerializer
+    queryset = User.objects.all()
