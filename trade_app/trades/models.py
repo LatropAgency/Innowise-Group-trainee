4867a901ce4bdf36c6d361221e496e082ce4d872
Kolesnikovich Vladimir <vovakolesnikovich@gmail.com>
2021-01-30 14:59:59 +0300

    Edited related_name and added __str__ methods

--- trade_app/trades/models.py
+++ trade_app/trades/models.py
@@ -27,40 +27,62 @@ class Currency(StockBase):
 class Item(StockBase):
     """Particular stock"""
     price = models.DecimalField(max_digits=7, decimal_places=2, blank=True, null=True)
-    currency = models.ForeignKey(Currency, blank=True, null=True, on_delete=models.SET_NULL, related_name="currency")
+    currency = models.ForeignKey(
+        Currency,
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name="item_currency"
+    )
     details = models.TextField("Details", blank=True, null=True, max_length=512)
+
+    def __str__(self):
+        return self.code
 
 
 class Inventory(models.Model):
     """The number of stocks a particular user has"""
-    user = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL, related_name="user")
-    item = models.ForeignKey(Item, blank=True, null=True, on_delete=models.SET_NULL, related_name="item")
+    user = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL, related_name="user_inventory")
+    item = models.ForeignKey(Item, blank=True, null=True, on_delete=models.SET_NULL, related_name="inventory_item")
     quantity = models.IntegerField("Stocks quantity", default=0)
+
+    def __str__(self):
+        return f'{self.user} {self.item.code}x{self.quantity}'
 
 
 class WatchList(models.Model):
     """Current user, favorite list of stocks"""
-    user = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL, related_name="user")
-    item = models.ForeignKey(Item, blank=True, null=True, on_delete=models.SET_NULL, related_name="item")
+    user = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL, related_name="watch_list_user")
+    item = models.ForeignKey(Item, blank=True, null=True, on_delete=models.SET_NULL, related_name="watch_list_item")
+
+    def __str__(self):
+        return f'{self.id} {self.item.code}'
 
 
 class Price(models.Model):
     """Item prices"""
-    currency = models.ForeignKey(Currency, blank=True, null=True, on_delete=models.SET_NULL, related_name="currency")
-    item = models.ForeignKey(Item, blank=True, null=True, on_delete=models.SET_NULL, related_name="item")
+    currency = models.ForeignKey(Currency, blank=True, null=True, on_delete=models.SET_NULL,
+                                 related_name="price_currency")
+    item = models.ForeignKey(Item, blank=True, null=True, on_delete=models.SET_NULL, related_name="price_item")
     price = models.DecimalField(max_digits=7, decimal_places=2, blank=True, null=True)
     date = models.DateTimeField(unique=True, blank=True, null=True)
+
+    def __str__(self):
+        return f'price: {self.price} {self.currency.code} date: {self.date}'
 
 
 class Offer(models.Model):
     """Request to buy or sell specific stocks"""
-    user = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL, related_name="user")
-    item = models.ForeignKey(Item, blank=True, null=True, on_delete=models.SET_NULL, related_name="item")
+    user = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL, related_name="offer_user")
+    item = models.ForeignKey(Item, blank=True, null=True, on_delete=models.SET_NULL, related_name="Offer_item")
     entry_quantity = models.IntegerField("Requested quantity")
     quantity = models.IntegerField()
     order_type = models.PositiveSmallIntegerField(choices=OrderType.items())
     price = models.DecimalField(max_digits=7, decimal_places=2, blank=True, null=True)
     is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f'user: {self.user} item: {self.item.code} {self.price} {self.order_type} status: {self.is_active}'
 
 
 class Trade(models.Model):
@@ -101,3 +123,6 @@ class Trade(models.Model):
         related_name="seller_offer",
         related_query_name="seller_offer",
     )
+
+    def __str__(self):
+        return f'seller: {self.seller} buyer: {self.buyer} item: {self.item.code}x{self.quantity}'
