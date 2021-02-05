bef227c8e36fa261f4cfd50450a22ad777357626
Kolesnikovich Vladimir <vovakolesnikovich@gmail.com>
2021-02-05 16:15:31 +0300

    Changed create function

--- trade_app/users/api/v1/serializers.py
+++ trade_app/users/api/v1/serializers.py
@@ -1,12 +1,13 @@
 from rest_framework import serializers
 from rest_framework_simplejwt.state import User
 
+from trades.models import Inventory
+
 
 class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = super().create(validated_data)
         user.set_password(validated_data['password'])
-        user.save()
         return user
 
     class Meta:
