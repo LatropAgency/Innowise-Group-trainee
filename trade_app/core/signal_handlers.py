4f04e920086b5ddfb4191720f68fdbd9f213e714
Kolesnikovich Vladimir <vovakolesnikovich@gmail.com>
2021-02-05 15:47:35 +0300

    Added user post_save signal which creates an empty inventory instance

---
+++ trade_app/core/signal_handlers.py
@@ -0,0 +1,10 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework_simplejwt.state import User
+
+from trades.models import Inventory
+
+
+@receiver(post_save, sender=User)
+def create_empty_inventory(sender, *args, **kwargs):
+    Inventory(user=sender).save()
